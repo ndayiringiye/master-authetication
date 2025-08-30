cb876d4da63c252c534e7c8140800e7cb0d5da85
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-08-30 13:06:11 +0200

    feat:/user password reseted successfully

--- backend/services/RequetChangePassword.js
+++ backend/services/RequetChangePassword.js
@@ -2,7 +2,7 @@ import User from "../models/userModel.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
 
-export const resetPassordService = async (req, res) => {
+export const requestResetPassord = async (req, res) => {
     const { email } = req.body;
     if (!email) {
         return res.status(400).json({ message: "Email is required" });
@@ -14,6 +14,9 @@ export const resetPassordService = async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
+        user.resetPassword= resetPassword;
+        user.resetPasswordExpire= Date.now() + 10 * 60 * 1000;
+        await user.save();
         const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
         const transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
@@ -32,6 +35,6 @@ export const resetPassordService = async (req, res) => {
         const info = await transporter.sendMail(mailsOptions);
         res.status(200).json({ message: "Reset email sent", info });
     } catch (error) {
-
+  res.status(500).json({ message: "Server error", error: error.message });
     }
 }
